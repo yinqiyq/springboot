e778321b404a46f3bfd60c7a074961b166dce3c1
yinqiyq <yinqiyq@163.com>
2020-05-13 23:58:45 +0800

    spring boot 热部署

--- src/main/java/com/how2java/springboot/Application.java
+++ src/main/java/com/how2java/springboot/Application.java
@@ -1,13 +1,13 @@
 package com.how2java.springboot;
-
+ 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
-public class SpringbootApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
-
+public class Application {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+ 
 }
--- src/main/java/com/how2java/springboot/web/HelloController.java
+++ src/main/java/com/how2java/springboot/web/HelloController.java
@@ -1,14 +1,21 @@
 package com.how2java.springboot.web;
-
+import java.text.DateFormat;
+import java.util.Date;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
-
+  
     @RequestMapping("/hello")
-    public String hello() {
-        return "Hello Spring Boot!";
+    public String hello(Model m) throws Exception {
+        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        if(true){
+            throw new Exception("some exception");
+        }
+        return "hello";
     }
-
+     
 }
